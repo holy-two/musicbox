eff67b85a7b86d75f14070a859d453a2e9747bcc
☘ <kogawananari@gmail.com>
2023-11-26 00:06:17 +0800

    :lipstick:

--- src/components/Player/synthController.ts
+++ src/components/Player/synthController.ts
@@ -4,9 +4,8 @@ class SynthController extends abcjs.synth.SynthController {
     get _randomAccess() {
         return (ev: PointerEvent) => {
             const button = ev.target as HTMLButtonElement;
-            const aside = document.querySelector("#root>aside") as HTMLDivElement;
             const background = button.classList.contains('abcjs-midi-progress-indicator') ? button.parentNode as HTMLElement : button;
-            const percent = Math.min(Math.max((ev.x - aside.offsetWidth - background.offsetLeft) / background.offsetWidth, 0), 1);
+            const percent = Math.min(Math.max((ev.x - background.getBoundingClientRect().left) / background.offsetWidth, 0), 1);
             const seek = Reflect.get(this, 'seek') as (percent: number) => void
             seek(percent)
             return Promise.resolve({ status: "ok" })
